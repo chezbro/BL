631f389c688bbda854e506a485f91d84f1fd545e
Eric Chesbrough <EricChesbrough@gmail.com>
2013-06-02 12:38:22 -0500

    More Homepage changes

--- app/models/person.rb
+++ app/models/person.rb
@@ -5,5 +5,10 @@ class Person < ActiveRecord::Base
 	has_many :guest_posts
 	has_many :trait_types, :through => :traits
 
-
+	#Validations
+	validates_presence_of :full_name, :dob, :dob, :eulogy, :photo_url
+	validates :eulogy, :length => { :maximum => 1000,
+    :too_long => "%{count} characters is the maximum length for a Eulogy" }
+    validates :eulogy, :length => { :minimum => 50,
+        :too_short => "%{count} characters is the minimum length for a Eulogy" }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,32 @@
 class User < ActiveRecord::Base
+
+	#Mass Assignment
 	attr_accessible :email, :password, :password_confirmation, :first_name, :last_name
+
+	#Authorization
+	before_filter :authorize_user, only: [:show, :edit, :update, :destroy]
+
+	def authorize_user
+	  @user = User.find(params[:id])
+	  if @user != current_user
+	    redirect_to users_url, notice: "Nice try."
+	  end
+	end
+
+
+	# Associations:
 	has_many :people
 	has_many :guest_posts
 
+	# Password
 	has_secure_password
-	validates :password, :presence => true, :on => :create
 
-	validates :email, :presence => true
-	validates :email, :uniqueness => true
-	validates :email, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create }
-
+	# Validations
+	validates :password, :presence => true, :on => :create, :length => { :in => 6..20 }
+	validates :email, :presence => true, :uniqueness => true, :format => { :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i, :on => :create }
 
 
+	#FB Connect
 	 def self.from_omniauth(auth)
 	  	where(auth.slice("provider", "uid")).first || create_from_omniauth(auth)
 	 end
